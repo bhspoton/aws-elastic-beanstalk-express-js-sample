7c497db3289f0169ce278c791be94125f559e894
bhspoton <54959961+bhspoton@users.noreply.github.com>
2021-01-14 20:25:08 -0600

    Fixed formating

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', function(req, res) {
+    res.send({"MSG":"Hello World!"});
+});
 
-app.listen(port) => console.log(`App running on http://localhost:${port}`)
+app.listen(port, function(err) {
+    console.log(`App running on http://localhost:${port}`);
+});
